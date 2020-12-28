62b96fb12895552a125ea4beb26685be67dc9f78
Matteo Di Gregorio <matteodgrgr@gmail.com>
2020-12-28 17:36:05 +0000

    adds possibility to save/load cards from/to file

--- deck.go
+++ deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 //create a new type of deck
 //a slice of strings
@@ -14,14 +19,39 @@ func newDeck() deck {
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, value + " of " + suit)
+			cards = append(cards, value+" of "+suit)
 		}
 	}
 	return cards
 }
 
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ",")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(fileName string) deck {
+	//"bs = byteSlice"
+	bs, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+
 }
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 func main()  {
-	cards := newDeck()
-
+	cards := newDeckFromFile("my-deck")
 	cards.print()
 }
